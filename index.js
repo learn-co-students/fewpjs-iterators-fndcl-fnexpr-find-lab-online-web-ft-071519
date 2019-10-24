877e43c3ab298aeb5ab3a23876e2a952520b66f2
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-24 18:56:42 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+let newarray= array.find(function(s){return s.result==="W"})
+if (newarray){
+
+ return newarray.year
+}else {
+  return newarray
+  
+}
+}
