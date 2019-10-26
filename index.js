17c381af84db62b8ef61bb80189987e50c72f0f3
Nicole Dow <nikki.dow71@gmail.com>
2019-10-26 16:42:46 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const ele = array.find(function(year){
+    return year['result'] === 'W'
+  })
+  if (ele) {
+    return ele['year']
+  }
+}
