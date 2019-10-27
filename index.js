c10cfac50028864d9da17d47eaac23a3a80c0394
radville <laura.radville@gmail.com>
2019-10-27 10:59:28 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let winner = record.find( function(s) { return s["result"] === "W"})
+  if (winner) {
+    return winner["year"]}
+}   
