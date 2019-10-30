1520f356ea1f01d38e848c42e388a140fe0d9d1c
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-10-30 13:02:58 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function winnerFn(el){
+  return el.result === "W"
+}
+
+function superbowlWin(array){
+    const winner = array.find(winnerFn)
+    return winner ? winner.year : undefined
+}
